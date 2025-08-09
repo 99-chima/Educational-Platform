2c66248672513f974605e2b0fcdfbbc06b04b48b
99-chima <chimaanjrini@gmail.com>
2025-08-09 13:31:44 +0300

    Update App.js

--- education-platform/src/App.js
+++ education-platform/src/App.js
@@ -6,20 +6,63 @@ import CoursesList from "./CoursesList";
 import About from "./About";
 import Testimonial from "./Testimonial";
 import Contact from "./Contact";
+import Footer from "./Footer";
+import Login from "./Login";
+import Checkout from "./Checkout";
 import './App.css';
+
 function App() {
   return (
     <CardProvider>
       <Router>
-        <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<CoursesList />} />
+          <Route
+            path="/login"
+            element={
+              <>
+                <NavBar />
+                <Login />
+                <Footer />
+              </>
+            }
+          />
+<Route path="/Checkout" element={
+  <>
+  <NavBar />
+  <Checkout />
+  <Footer />
+  </>
+}
+/>
+          <Route
+            path="/"
+            element={
+              <>
+                <NavBar />
+                <Home />
+               
+                <About />
+                <Testimonial />
+                <Contact />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/courses"
+            element={
+              <>
+                <NavBar />
+                <CoursesList />
+                <About />
+                <Testimonial />
+                <Contact />
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </Router>
-      <About />
-  <Testimonial />
-      <Contact />
     </CardProvider>
   );
 }
